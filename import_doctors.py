70a3f93954bb12ca19c06d98550afe0c2491b96c
Simon Michalke <simon@clundxiii.com>
2020-03-21 12:30:53 +0100

    cleaned up code

--- import_doctors.py
+++ import_doctors.py
@@ -1,44 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
-
-# In[2]:
-
 
 import json
 import urllib.request
 
 import pandas as pd
 
-
-# In[9]:
-
-
 import_url_doctors = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/Arzt_doctors/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
 
 with urllib.request.urlopen(import_url_doctors) as response:
     data = json.loads(response.read().decode())
 
-
-# In[16]:
-
-
 processed_data = [{**item["attributes"], **item["geometry"]} for item in data["features"]]
-
-
-# In[19]:
-
 
 df_doctors = pd.DataFrame(processed_data)
 
-
-# In[80]:
-
-
 df_doctors_clean = df_doctors[~df_doctors.name.isna()]
-
-
-# In[101]:
-
 
 df_doctors_clean = df_doctors_clean[
     ~df_doctors_clean.address_city.isna()
@@ -62,9 +39,5 @@ df_import.lat = df_doctors["y"]
 df_import.long = df_doctors["x"]
 df_import.comment = "TODO"
 
-
-# In[106]:
-
-
-df_import.to_sql("doctors.csv", index=False)
+df_import.to_csv("doctors.csv", index=False)
 
