54ed6c22d98ad94a8f46d6a46993e7fa5e599829
Simon Sudrich <simon.sudrich@daimler.com>
2020-03-21 15:57:31 +0100

    fix timestamp again

--- import_resources.py
+++ import_resources.py
@@ -15,11 +15,11 @@ resource_types = ["Betten", "IPS-Betten ohne Beatmung", "IPS-Betten mit Beatmung
 possible_max_capacities = range(100, 525, 25)
 
 max_capacities = [(institution_id, resource_type, random.choice(possible_max_capacities)) for institution_id in institution_ids for resource_type in resource_types]
-resources = [(institution_id, resource_type, max_cap, random.choice(range(max_cap + 1)), int(datetime.now().timestamp())) for institution_id, resource_type, max_cap in max_capacities]
+resources = [(institution_id, resource_type, max_cap, random.choice(range(max_cap + 1)), datetime.now()) for institution_id, resource_type, max_cap in max_capacities]
 
 column_names = ["institution_id", "resource_type", "current_capacity", "max_capacity", "timestamp"]
 
-df_import = pd.DataFrame(resources, columns=column_names)
+df_import = pd.DataFrame(resources, columns=column_names, dtype=)
 
 with open ("connection", "r") as myfile:
     conStr = myfile.readlines()[0]
