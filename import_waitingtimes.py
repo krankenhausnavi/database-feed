2c26f53b525dbc2dacc2aeb97f89cf6ab6ea3c84
octycs <git@octycs.eu>
2020-03-21 14:30:19 +0100

    Add random waiting times

---
+++ import_waitingtimes.py
@@ -0,0 +1,25 @@
+
+#!/usr/bin/env python3
+# coding: utf-8
+import random
+from datetime import datetime
+import pandas as pd
+
+n_institutions = 4000
+
+institution_ids = range(n_institutions)
+service_types = {"Hausbesuch", "Notfalltermin", "Telefonauskunft"}
+
+column_names = ["institution_id", "service_type", "waiting_time", "timestamp"]
+
+waiting_times = [(i_id,
+                  s_type,
+                  random.randint(0,60)*5,
+                  datetime.now().timestamp())
+                 for s_type in service_types
+                 for i_id in institution_ids
+                 if random.random() < 0.5]
+
+df_import = pd.DataFrame(waiting_times, columns=column_names)
+
+# TODO: Add to DB
